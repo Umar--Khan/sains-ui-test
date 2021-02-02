d898c58cc692ee3daf4c9beab5a38c4f27c1f187
Umar--Khan <umarkhandev@gmail.com>
2021-02-02 21:22:47 +0000

    chore: removed default css and public files cleanup

---
+++ src/Root.tsx
@@ -0,0 +1,18 @@
+const Root = () => (
+    <div>
+      <header>
+        <p>
+          Edit <code>src/Root.tsx</code> and save to reload.
+        </p>
+        <a
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+
+export default Root;
